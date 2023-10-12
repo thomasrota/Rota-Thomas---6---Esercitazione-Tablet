bfdd07b006ede051e7421cbc23f9e0c3ef5ad6a1
rota.20196 <rota.20196@E2-9-PC11>
2023-10-12 10:13:34 +0200

    Aggiungere i file di progetto.

---
+++ Rota Thomas - 6 - Esercitazione Tablet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rota_Thomas___6___Esercitazione_Tablet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
